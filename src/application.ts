a0c88832a973a6a288ca7aa991641f2ca575e9c1
trungnt <nttrung143@gmail.com>
2016-10-24 10:54:46 +0700

    add body-parser, cookie-parser

--- src/application.ts
+++ src/application.ts
@@ -4,6 +4,9 @@ import express = require("express");
 import rootRouter = require("./routes/rootRouter");
 import requestLogger = require("./middleware/requestLogger");
 import Config from "./config/config";
+let path = require("path");
+let cookieParser = require("cookie-parser");
+let bodyParser = require("body-parser");
 
 export class App {
     /**
@@ -12,6 +15,7 @@ export class App {
      */
 
     constructor(private app: express.Express, private config: Config) {
+        this.configureApp(app);
         this.configureMiddleware(app);
         this.configureRoutes(app);
     }
@@ -19,6 +23,18 @@ export class App {
     /**
      * @param app - express application
      */
+    private configureApp(app: express.Express) {
+        // view engine setup
+        app.set("views", path.join(this.config.root, "views"));
+        app.set("view engine", "ejs");
+
+        // bodyParser
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+
+        // cookieParser
+        app.use(cookieParser());
+    }
 
     private configureMiddleware(app: express.Express) {
         app.use(requestLogger);
